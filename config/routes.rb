fc816956bb9b3c6ca8e9a659b2d69ecc5f38be98
github username <meliajm@gmail.com>
2020-03-13 20:52:32 -0700

    added routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#home'
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  
 end
